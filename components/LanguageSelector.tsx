600f631f5f23f56554f0f2f3ec0ebb5d590434dd
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-08-11 21:40:22 +0530

    dropdown added

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import React from 'react'
+import { languages } from '@/utils/utilities';
+import { ChevronDown } from 'lucide-react';
+import React, { useState } from 'react'
 
 interface LanguageSelectorProps{
   language : string;
@@ -13,9 +15,42 @@ function LanguageSelector({
   setLanguage,
   setActiveIcon
 }) {
+
+  const [showDropdown, setShowDropdown] = useState(false)
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown)
+  }
+
+  const handleLanguageChange = (newLanguage : string) => {
+    setLanguage(newLanguage)
+
+    const newActiveIcon = languages.find((lang) => lang.name === newLanguage)?.icon
+
+    if (newActiveIcon) {
+      setActiveIcon(newActiveIcon)
+    }
+  }
+
+
   return (
-    <div>
-      <p>Language</p>
+    <div onClick = {toggleDropdown}>
+      <p className = 'py-[5px] text-sm font-medium'>Language</p>
+      <div className = 'dropdown-title capitalize w-[120px]'>
+        {language}
+        <ChevronDown/> 
+      </div>
+      {showDropdown && 
+        <div className = 'dropdown-menu'>
+          {languages.map((lang , index) => {
+            return <div key = {index}>
+              <button className = 'dropdown-item w-[120px] top-[74px] text-left' onClick = {() => handleLanguageChange(lang.name)}>
+                {lang.name}
+              </button>
+            </div>
+          })}
+        </div>
+      }
     </div>
   )
 }
