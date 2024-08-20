a1a8f0929b6e814f4ea9c7f14bfcebb1ab6013c5
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-08-21 04:41:31 +0530

    make changes in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 import BackgroundSelector from "@/components/BackgroundSelector";
 import CodeEditor from "@/components/CodeEditor";
+import Footer from "@/components/Footer";
 import LanguageSelector from "@/components/LanguageSelector";
 import PaddingSelector from "@/components/PaddingSelector";
 import ThemeSelector from "@/components/ThemeSelector";
 import { backgrounds, languages, themes } from "@/utils/utilities";
-import { useState } from "react";
+import { ArrowDownToLine } from "lucide-react";
+import { useRef, useState } from "react";
+import html2canvas from "html2canvas";
 
 export default function Home() {
+
+  const editorRef = useRef(null)
 
   const [language , setLanguage] = useState(languages[0].name)
   const [activeIcon, setActiveIcon] = useState(languages[0].icon)
@@ -16,19 +21,70 @@ export default function Home() {
   const [paddings, setPaddings] = useState(["1rem", "2rem", "3rem", "4rem"])
   const [currentPadding, setCurrentPadding] = useState(paddings[2])
 
+  const exportPng = async() => {
+    const editorElem = editorRef.current
+
+    if (editorElem) {
+      //hide elemnets
+      const handleElems = document.querySelectorAll(".handle") as any;
+      const cursorElem = document.querySelector(".ace_cursor") as any;
+      const codetitle = document.querySelector(".code-title") as any;
+      const codeEditor = document.querySelector(".ace_editor") as any;
+
+      handleElems.forEach((elem: any) => {
+        elem.style.display = "none";
+      });
+      cursorElem.style.display = "none";
+      codetitle.style.boxShadow = "none";
+      codeEditor.style.boxShadow = "none";
+
+      const canvas = await html2canvas(editorElem)
+      const image = canvas.toDataURL("image/png").replace("image/png" , "image/octet-stream")
+
+      const link = document.createElement("a")
+      link.download = "code.png"
+      link.href = image
+      link.click()
+
+      //show elements
+      handleElems.forEach((elem: any) => {
+        elem.style.display = "block";
+      });
+      cursorElem.style.display = "block";
+      codetitle.style.boxShadow = "0 3px 10px rgba(0, 0, 0, 0.2)";
+      codeEditor.style.boxShadow = "2px 3px 10px rgba(0, 0, 0, 0.2)";
+    }
+
+  }
+
   return (
     <main className="h-[100vh] flex flex-col items-center justify-between">
+      
       <header className="mt-6 w-[940px] flex gap-6 p-5 fixed top-0 left-1/2 translate-x-[-50%] z-10 bg-[#191919] rounded border border-[#3C3C3C] shadow-md">
+      
         <LanguageSelector language = {language} setLanguage = {setLanguage} setActiveIcon = {setActiveIcon} />
+      
         <ThemeSelector theme={theme} setTheme = {setTheme}/>
+      
         <BackgroundSelector background = {background} setBackground = {setBackground} />
+      
         <PaddingSelector paddings = {paddings} currentPadding = {currentPadding} setCurrentPadding = {setCurrentPadding}/>
-        <div className="export-btn"></div>
-        {/* 2hr 17min 17sec */}
+      
+        <div className="export-btn self-center ml-auto">
+          <button className="flex items-center gap-2  bg-blue-400 py-2 px-3 text-blue-400 text-sm rounded-md font-medium bg-opacity-10 hover:bg-opacity-80 hover:text-slate-50 ease-in-out transition-all duration-300"
+          onClick={exportPng}>
+            Export png 
+            <ArrowDownToLine />
+          </button>
+        </div>
+      
       </header>
-      <div className="code-editor-ref mt-[14rem]">
+      
+      <div className="code-editor-ref mt-[14rem]" ref={editorRef}>
         <CodeEditor language = {language} theme = {theme} icon = {activeIcon} background = {background} currentPadding = {currentPadding}/>
       </div>
+      
+      <Footer/>
     </main>
   );
 }
