48fddf6a08f75420aa683815c8c0219f715fc480
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-08-23 00:47:40 +0530

    project completed

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -79,6 +79,20 @@ function CodeEditor({
     style = {{background : background}}>
 
       <div className="code-block" style={{padding: currentPadding}}>
+
+        <div
+            className="handle handle-top absolute left-1/2 translate-x-[-50%] top-[-4px] w-2 h-2 
+              rounded-full bg-slate-300 hover:bg-slate-50"></div>
+          <div
+            className="handle handle-bottom absolute left-1/2 bottom-[-4px] w-2 h-2 rounded-full
+          bg-slate-300 hover:bg-slate-50 "></div>
+          <div
+            className="handle handle-left absolute left-[-4px] top-1/2 w-2 h-2 rounded-full 
+          bg-slate-300 hover:bg-slate-50 "></div>
+          <div
+            className="handle handle-right absolute right-[-4px] top-1/2 w-2 h-2 rounded-full
+          bg-slate-300 hover:bg-slate-50 "></div>
+
         <div className="code-title h-[52px] px-4 flex items-center justify-between bg-black bg-opacity-80">
           <div className="dots flex items-center gap-1">
             <div className='w-3 h-3 rounded-full bg-[#ff5656]'></div>
@@ -87,7 +101,7 @@ function CodeEditor({
           </div>
 
           <div className="input-control w-full">
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='w-full text-[hsla(0,0%,100%,0.6)] outline-none font-medium text-center bg-transparent' />
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='w-full text-[hsla(0,0%,100%,0.6)] outline-none font-medium text-center bg-transparent' style={{lineHeight: "1.8rem"}} />
           </div>
 
           <div className="icon flex justify-center items-center bg-black p-1 bg-opacity-30 rounded-sm">
